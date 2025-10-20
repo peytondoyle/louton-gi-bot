64e5af75842a5d1704333e7e7d9acf051f48bee6
Peyton Doyle <p6doyle@gmail.com>
2025-10-20 08:12:19 -0400

    Remove ALL Nix config - use Replit built-in Node (simplest approach)

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }:
-{
-  deps = [
-    pkgs.nodejs-20_x          # Node 20 with npm bundled
-    pkgs.bashInteractive      # bash
-    pkgs.git                  # git (auto-deploy script uses it)
-    pkgs.cacert               # TLS certs for HTTPS requests
-  ];
-}
